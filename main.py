ff49f62400c591a0f998bf0c5e2816601526a40a
Nathan Souza <naathanmarinho11@gmail.com>
2024-06-22 11:38:30 -0300

    adiciona analise estatica

--- main.py
+++ main.py
@@ -10,14 +10,19 @@ def palavra_num(palavra):
     }
     return palavra_para_num.get(palavra, 1)
 
+
 def converte_numerico(valor):
-    correspondencias = re.findall(r'(\d+(?:,\d+)?)(?:\s+(milhões?|mil))?', valor)
+    correspondencias = re.findall(
+        r'(\d+(?:,\d+)?)(?:\s+(milhões?|mil))?', valor)
     for correspondencia in correspondencias:
-        numero = float(correspondencia[0].replace(',', '.')) * palavra_num(correspondencia[1])
+        numero = float(correspondencia[0].replace(
+            ',', '.')) * palavra_num(correspondencia[1])
         valor = valor.replace(' '.join(correspondencia), str(numero))
     return float(valor.replace(',', '.'))
 
-parametros.df['quantidade_ou_valor'] = parametros.df['quantidade_ou_valor'].apply(converte_numerico)
+
+parametros.df['quantidade_ou_valor'] = parametros.df[
+    'quantidade_ou_valor'].apply(converte_numerico)
 pd.options.display.float_format = '{:.2f}'.format
 
 print(parametros.df)
